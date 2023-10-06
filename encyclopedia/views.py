b8be0f7c3d8f1e7cf1b062bd834b0f8a3f3893b3
ninjoshATTACK <joshua.gatpandan@gmail.com>
2023-10-06 13:36:21 -0800

    Search function works properly now

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -37,29 +37,30 @@ def entry(request, title):
             "title": title, "content": htmlfile, "form": form
         })
 
-# Search NOT WORKING YET
+# Search
 def search(request):
-    if request.method == "POST":
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            input = form.cleaned_data.get("query")
+    if request.method == "GET":
+        q = request.GET.get("q","")
+
+        if q != "":
             showentry = False
             for entry in util.list_entries():
                 # Search result was found
-                if input == entry:
-                    mdfile = util.get_entry()
+                if q.lower() == entry.lower():
+                    mdfile = util.get_entry(q)
                     htmlfile = markdowner.convert(mdfile)
                     showentry = True
                     break
             # Display entry if found
             if showentry:
                 return render(request, "encyclopedia/entry.html", {
-                    "title": entry, "content": htmlfile, "form": form
+                    "title": entry, "content": htmlfile, "form": q
                 })
             else:
                 results = []
                 for entry in util.list_entries():
-                    if input in entry:
+                    # To see if substring is found in entries
+                    if q.lower() in entry.lower():
                         results.append(entry)
                 # Substring was not found in all the entries
                 if len(results) == 0:
@@ -71,7 +72,7 @@ def search(request):
                 # The substring was found in these entries
                 else:
                     return render(request, "encyclopedia/search.html", {
-                        "entries": results, "form": form
+                        "entries": results, "form": q
                     })
     else:
         form = SearchForm()
