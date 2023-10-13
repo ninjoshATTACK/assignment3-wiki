ea7e1f2d2180887612c11ce586fd66fdbc8468f3
acslmarc <marc@chezbrown.org>
2023-10-12 19:40:38 -0800

    See email with your grade; great job!

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django import forms
 from markdown2 import Markdown
 import random
@@ -32,18 +32,16 @@ def index(request):
 def entry(request, title):
     e = util.get_entry(title)
     if e is None:
-        form = SearchForm()
-        content = "The entry you are trying to access does not exist."
+        msg = "The entry you are trying to access does not exist."
         return render(request, "encyclopedia/error.html", {
-            "form": form, "content": content
+            "error_message": msg
         })
     else:
-        form = SearchForm()
         mdfile = util.get_entry(title)
         # Convert md to html
         htmlfile = markdowner.convert(mdfile)
         return render(request, "encyclopedia/entry.html", {
-            "title": title, "content": htmlfile, "form": form
+            "title": title, "content": htmlfile
         })
 
 # Search
@@ -56,15 +54,11 @@ def search(request):
             for entry in util.list_entries():
                 # Search result was found
                 if q.lower() == entry.lower():
-                    mdfile = util.get_entry(q)
-                    htmlfile = markdowner.convert(mdfile)
                     showentry = True
                     break
             # Display entry if found
             if showentry:
-                return render(request, "encyclopedia/entry.html", {
-                    "title": entry, "content": htmlfile, "form": q
-                })
+                return redirect('entry', title=entry)
             else:
                 results = []
                 for entry in util.list_entries():
@@ -73,10 +67,8 @@ def search(request):
                         results.append(entry)
                 # Substring was not found in all the entries
                 if len(results) == 0:
-                    form = SearchForm()
-                    content = "The page you searched for does not exist."
                     return render(request, "encyclopedia/error.html", {
-                        'form': form, "content": content
+                        'error_message': "The page you searched for does not exist."
                     })
                 # The substring was found in these entries
                 else:
@@ -84,10 +76,9 @@ def search(request):
                         "entries": results, "form": q
                     })
     else:
-        form = SearchForm()
         content = "No search result was found."
         return render(request, "encyclopedia/error.html", {
-            'form': form, 'content': content
+            'error_message':"No search result was found."
         })
 
 # Create a New Entry Page
@@ -105,20 +96,13 @@ def create(request):
                     break
             # Title is already in entries
             if exists:
-                content = "This page already exists."
-                form = SearchForm()
                 return render(request, "encyclopedia/error.html", {
-                    'form': form, "content": content
+                    'error_message': "This page already exists."
                 })
             # Title doesn't exist so save the entry
             else:
                 util.save_entry(title, body)
-                form = SearchForm()
-                mdfile = util.get_entry(title)
-                htmlfile = markdowner.convert(mdfile)
-                return render(request, "encyclopedia/entry.html", {
-                    "title": title, "content": htmlfile, "form": form
-                })
+                return redirect('entry', title=title)
     else:
         form = SearchForm()
         createform = CreateForm()
@@ -152,9 +136,4 @@ def randomEntry(request):
     length = len(entries)
     chosen = random.randint(0, length-1)
     title = entries[chosen]
-    mdfile = util.get_entry(title)
-    htmlfile = markdowner.convert(mdfile)
-    form = SearchForm()
-    return render(request, "encyclopedia/entry.html", {
-        "title": title, "content": htmlfile, "form": form
-    })
+    return redirect('entry', title=title)
